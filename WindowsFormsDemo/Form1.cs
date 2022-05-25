5a0d44342ad3b023d4dae5dc0a13219c75543f77
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:55:31 +0530

    first commit

---
+++ WindowsFormsDemo/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form is loaded");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result=MessageBox.Show("Sure to close the form?","Exit",MessageBoxButtons.YesNoCancel);
+            if(result!=DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
